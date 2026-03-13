cbc47e8402acff14b3ea821ee7d4c98685e1c31a
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-03-12 22:37:40 -0500

    tema 3 primera parte

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,123 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Tema 3
+      h1 Análisis de información
+  
+    .fullwidth-bg.fullwidth-bg--1
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-md-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema3/1.png", alt="")
+      .col-md-10.col-lg-8
+        p.mb-3 El análisis permite contextualizar la información suministrada por la medición y entender la causa y el efecto de las acciones desarrolladas en la web. Una vez se haya realizado la medición es necesario, entonces, analizar la información obtenida, en este punto es importante saber qué es lo que se debe analizar, puesto que la analítica arroja un sinfín de datos con los que se debe establecer un filtro con el fin de optimizar tiempo y recursos.
+        .p-3.bg-acento-contenido-a5
+          .row.justify-content-center.align-items-center
+            .d-none.d-xl-block.col-xl-1
+              figure
+                img.img-65(data-aos="fade-down-right", src="@/assets/curso/tema2/16.svg", alt="")
+            .col-xl-11
+              p.mb-0 Para saber qué es lo realmente importante en el momento de analizar, se debe tener en cuenta lo siguiente:
+
+    .row.justify-content-center.align-items-stretch.custom-tarjeta-numerada.custom-tarjeta-numerada__mod.mb-4.mb-lg-5
+      .col-md-10.col-lg-6.col-xl-5.my-4.mb-5.my-lg-0
+        .tarjeta-numerada.color-secundario.p-2.ps-5.h-100.bg-azul-claro.position-relative
+          .tarjeta-numerada__numero
+            .h2 1
+          p.mb-0.text-bold.w-75 Medir solo lo que se necesite para alcanzar los objetivos de negocio.
+          img.d-none.d-md-block.img-113.position-absolute.icon-tarjeta-numerada(src="@/assets/curso/tema3/2.svg", alt="")
+      .col-md-10.col-lg-6.col-xl-5
+        .tarjeta-numerada.color-secundario.p-2.ps-5.h-100.bg-azul-claro.position-relative
+          .tarjeta-numerada__numero
+            .h2 2
+          p.mb-0.text-bold.w-75 Analizar los datos sin perder el foco del porqué se hace.
+          img.d-none.d-md-block.img-113.position-absolute.icon-tarjeta-numerada(src="@/assets/curso/tema3/3.svg", alt="")
+
+    .row.justify-content-center
+      .col-xl-10
+        .p-3.bg-acento-botones-a20.rounded-3
+          p.mb-0.text-bold La idea de analizar los datos es para responder a preguntas que sugiere Cibrián (2019, p. 79). Ellas son:
+
+    .row.justify-content-center.align-items-center.z-2.mb-5
+      .col-xl-10
+        .titulo-sexto.color-acento-contenido.mb-4
+          h5 Figura 1.
+          span Titulo de la figura
+        img.mb-2.d-sm-none(data-aos="fade-up", src="@/assets/curso/tema3/5-mob.svg", alt="pendiente por accesibilidad")
+        img.mb-2.d-none.d-sm-block(data-aos="fade-up", src="@/assets/curso/tema3/5.svg", alt="pendiente por accesibilidad")
+        figcaption #[strong Nota.] SENA, 2026
+
+    .bg-azul-claro.p-2
+      .row.justify-content-center.align-items-center
+        .d-none.d-lg-block.col-lg-2
+          figure
+            img(data-aos="fade-down-right", src="@/assets/curso/tema3/6.svg", alt="")
+        .col-lg-9
+          .p-4.p-lg-0
+            p.mb-0 Al aterrizar los datos con el contexto es cómo se encuentra información relevante también llamada insights, que permitirán una toma de decisiones concienzuda y entendida según la realidad. Es importante comprender que el contexto les da sentido a las conclusiones: ¿en qué contexto se hará la intervención?, ¿quiénes están en ese contexto?, ¿qué les gusta?… Se sugiere no pensar tanto y atreverse a reconocer el contexto.
+
+    Separador
+    #t_3_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 3.1 ¿Qué se debe medir?
+
+    .fullwidth-bg.fullwidth-bg--1
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-md-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema3/7.png", alt="")
+      .col-md-10.col-lg-8
+        p.mb-3 Haciendo énfasis en la importancia de poner en contexto los datos encontrados y para realizar un mejor análisis de la información obtenida, se necesita entender el mercado en el que se desenvuelve el negocio, segmentar el público y poner especial cuidado en los KPI utilizados según los objetivos establecidos, puesto que en un mar de datos es posible perder el norte, si no se ha definido qué se desea y se debe analizar.
+
+    .p-3.bg-acento-botones-a20.rounded-3.z-2.mb-4.mb-lg-5
+      p.mb-0 Según Cibrián (2019) es muy útil el uso de dos componentes que permitirán contextualizar los datos suministrados por la medición, a partir de ellos es como se podrá dar claridad y entendimiento a los resultados que está arrojando la estrategia de marketing aplicada y determinar si es la más adecuada o debe ser modificada para lograr los objetivos propuestos.
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-10
+        p.mb-0.text-center.text-bold Ahora, se debe revisar con atención la siguiente información que agrega contenido relevante a este proceso de aprendizaje.
+
+    .row.justify-content-center
+      .col-md-10.col-lg-8.col-xl-6.mb-4.mb-xl-0
+        .crd_hover_txt.rounded-4.overflow-hidden(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/tema3/8.png", alt="Imagen de acompañamiento")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Reportes automáticos
+            p.mb-0 Informes programados (diarios, semanales o mensuales) que consolidan los indicadores y permiten hacer seguimiento a las metas y tendencias.
+
+      .col-md-10.col-lg-8.col-xl-6
+        .crd_hover_txt.rounded-4.overflow-hidden(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/tema3/9.png", alt="Imagen de acompañamiento")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Panel de control
+            p.mb-0 Comparación de evolución de los datos generados con respecto a una línea de tiempo, es decir, ver cómo se comportan los datos encontrados a lo largo del tiempo, con el fin de darlos a conocer en gráficos que permitan identificar cambios significativos para la toma de decisiones. Esta representación de datos permite observar desde un plano general, para ubicar hechos específicos del porqué suceden y en qué están afectando la consecución de los objetivos propuestos.
+
+    Separador
+    #t_3_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 3.2 Análisis de la competencia
+
+    .fullwidth-bg.fullwidth-bg--1
+    img(data-aos="fade-up")(src="@/assets/curso/tema3/10.png", alt="Imagen decorativa")
+
+    .row.justify-content-center.z-2
+      .col-lg-10.p-4.drop-shadow(data-aos="fade-down")
+        div.banner-text.banner-text--azul
+          p.mb-0 Para saber direccionar los esfuerzos encaminados a lograr los objetivos propuestos, se debe conocer a la competencia, esto permite entender el escenario en el que se desenvuelve una marca o una empresa, las oportunidades del mercado, las posibles amenazas y es de gran ayuda para direccionar las estrategias establecidas.
+
+    .row.justify-content-center.align-items-center.mb-4.mb-lg-5.z-2
+      .col-md-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema3/11.png", alt="")
+      .col-md-10.col-lg-8
+        p.mb-3 Aunque en la cotidianidad las comparaciones nunca son buenas, en el marketing digital pueden ser muy provechosas para poner los pies en la tierra y dirigirse por el camino correcto, Con respecto a esto, según Maciá y Santoja (2017) se debe hacer uso de la matriz DOFA (debilidades, oportunidades, fortalezas y amenazas) para observar estos aspectos con relación a otra marca, igualmente se deben plantear interrogantes en torno a las redes sociales utilizadas, los contenidos, la periodicidad de publicación, las interacciones de los usuarios, cantidad de seguidores, tono que usan para comunicarse, publicaciones populares, es decir, toda la información que se puede obtener al revisar la red social externamente.
+
+    .p-3.bg-acento-botones-a20.rounded-3.z-2.mb-4.mb-lg-5
+      p.mb-0 Según Cibrián (2019) es muy útil el uso de dos componentes que permitirán contextualizar los datos suministrados por la medición, a partir de ellos es como se podrá dar claridad y entendimiento a los resultados que está arrojando la estrategia de marketing aplicada y determinar si es la más adecuada o debe ser modificada para lograr los objetivos propuestos.
+    
+    Separador
+    #t_3_3.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 3.3 Titulo de la sección
 
 </template>
 
--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,8 +5,104 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 Tema 4
+      h1 Estructuración e interpretación de indicadores para la preparación de reportes
 
+    .row.justify-content-center.align-items-center.mb-4.z-2
+      .col-md-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema4/1.png", alt="")
+      .col-md-10.col-lg-8
+        p.mb-0 De nada sirve medir cifras y analizar datos si no se plasman en información importante para direccionar la toma de decisiones. Con esto, se hace evidente la necesidad de usar indicadores que guíen la presentación de dicha información para ser trabajada con el equipo técnico.
+
+    .fullwidth-bg.fullwidth-bg--1
+    .p-4.bg-acento-botones-a10.rounded-3.z-2
+      p.mb-0 Según la #[em Guía para diseño, construcción e interpretación de indicadores ](DANE, s.f.), un indicador se considera como la expresión cualitativa o cuantitativa capaz de ser observada y con la que es posible identificar comportamientos o características de una variable con relación a un tiempo determinado, encaminada a la meta fijada; para ello, el indicador debe tener simplificación, medición y comunicación. 
+      
+    Separador
+
+    #t_4_1.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 4.1 Características de un reporte de resultados
+
+    .row.justify-content-center.align-items-center.mb-4.z-2
+      .col-md-10.col-lg-4
+        figure
+          img.mb-4.mb-lg-0(data-aos="fade-down-right", src="@/assets/curso/tema4/2.png", alt="")
+      .col-md-10.col-lg-8
+        p.mb-5.mb-sm-0 Un informe de resultados basa sus características de acuerdo con la persona que será presentada como también definirá los KPI, objetivos, formatos, etc. Es importante saber a quién se dirige para exponer de la manera más clara la información encontrada y permitir una toma de decisiones certera. Según Cibrián (2019), existen cuatro requisitos para un buen reporte de resultados:
+        .row.justify-content-center.align-items-center.custom-tarjeta-numerada
+          .col-3.d-none.d-xl-block
+            figure
+              img(src="@/assets/curso/tema4/3.png", alt="")
+          .col-xl-9
+            .row.justify-content-center.align-items-stretch
+              .col-md-6.mb-4.my-md-5
+                .tarjeta-numerada.color-secundario.p-3.ps-5.h-100.bg-azul-claro
+                  .tarjeta-numerada__numero
+                    .h2 1
+                  p.mb-0.text-bold Información relevante y actualizada.
+              .col-md-6.mb-4.my-md-5
+                .tarjeta-numerada.color-secundario.p-3.ps-5.h-100.bg-azul-claro
+                  .tarjeta-numerada__numero
+                    .h2 2
+                  p.mb-0.text-bold Fácil comprensión visual.
+              .col-md-6.mb-4
+                .tarjeta-numerada.color-secundario.p-3.ps-5.h-100.bg-azul-claro
+                  .tarjeta-numerada__numero
+                    .h2 3
+                  p.mb-0.text-bold Conclusiones y recomendaciones.
+              .col-md-6.mb-4
+                .tarjeta-numerada.color-secundario.p-3.ps-5.h-100.bg-azul-claro
+                  .tarjeta-numerada__numero
+                    .h2 4
+                  p.mb-0.text-bold Automatización
+
+    
+    .cajon.color-terciario.p-4
+      p.mb-0 La finalidad de un informe bien estructurado radica en su entendimiento de manera rápida y eficaz, dicho en otras palabras, debe ser explicada en un mismo idioma para todos los receptores, permitiendo desarrollar conclusiones a primera vista para ser analizadas a profundidad y direccionar la estrategia.
+
+    Separador
+    #t_4_2.titulo-segundo.color-acento-contenido(data-aos="flip-up")
+      h2 4.2 Tipos de reportes
+
+    .bloque-texto-g.color-acento-contenido.p-3.p-sm-4.p-md-5.mb-4.mb-lg-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/tema4/4.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Teniendo en cuenta la información que se desea dar a conocer, se pueden definir tres tipos de reportes, cuya importancia radica en aprender a organizar la información encontrada para exponerla de manera clara y tener muy presente para la toma de decisiones (Cibrián, 2019). Ellos son:
+
+    .row.justify-content-center.mb-4.mb-lg-5
+      .col-xl-4.col-sm-8.col-lg-6.col-11.mb-4.mb-xl-0
+        .crd_hover_txt.rounded-4.overflow-hidden(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/tema4/5.png", alt="Imagen de acompañamiento")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Cuadro de mando
+            p.mb-0 Recolección de datos asociados de los usuarios que interactúan con la red social o sitio web.
+
+      .col-xl-4.col-sm-8.col-lg-6.col-11.mb-4.mb-xl-0
+        .crd_hover_txt.rounded-4.overflow-hidden(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/tema4/6.png", alt="Imagen de acompañamiento")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Panel de control
+            p.mb-0 Estudio y comprensión de los datos encontrados, segmentados de acuerdo con los criterios definidos.
+
+      .col-xl-4.col-sm-8.col-lg-6.col-11.mb-4.mb-xl-0
+        .crd_hover_txt.rounded-4.overflow-hidden(data-aos="flip-left")
+          .crd_hover_txt--img
+            figure
+              img(src="@/assets/curso/tema4/7.png", alt="Imagen de acompañamiento")
+          .crd_hover_txt--body
+            h4.mb-3.text-center Informe
+            p.mb-0 Acción de plasmar por medio de gráficos los resultados encontrados, con el fin de una toma de decisiones adecuada.
+
+    .cajon.color-acento-botones.p-4
+      p.mb-0 Al finalizar la lectura de este componente de formación, se espera que se haya recuperado información para desarrollar conocimientos, habilidades y actitudes que deberán aplicarse al contexto laboral, pues es allí en donde tiene sentido esta experiencia de aprendizaje.
+
+    
 
 </template>
 
